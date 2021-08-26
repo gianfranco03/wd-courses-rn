2f71d889c281b2272e809db550065384b145d2d9
RudyCancax <rudycancax@gmail.com>
2021-08-26 17:46:28 -0600

    Archivo sin utilizar supabaseClient.js eliminado

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_KEY } from 'react-native-dotenv';
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
