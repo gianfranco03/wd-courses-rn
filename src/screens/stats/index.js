259a5162d42593c7af490b0bd195b79347e00e06
Hans <hgirondl@gmail.com>
2021-08-26 20:12:31 -0600

    Graficos sin conexion

--- src/screens/stats/index.js
+++ src/screens/stats/index.js
@@ -1,183 +1,148 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {View, Text, Dimensions, ScrollView} from 'react-native';
-import {LineChart, ProgressChart, PieChart} from 'react-native-chart-kit';
-import {useTheme, Appbar} from 'react-native-paper';
+import {useTheme, Appbar, Title} from 'react-native-paper';
+import {
+  VictoryPie,
+  VictoryChart,
+  VictoryBar,
+  VictoryLabel,
+  VictoryAxis,
+} from 'victory-native';
 
-import ListAccordion from '../../components/listAccordion';
+import {Tabs, TabScreen} from 'react-native-paper-tabs';
 
-import {supabase} from '../../lib/constants/api';
-import {getLineCharData} from '../../utils/toast/chartDataBuilder';
-import styles, {containerWidth} from './styles';
+import styles from './styles';
 
-const screenWidth = Dimensions.get('window').width - containerWidth;
-
-export const filter_map = {
-  course: 'course',
-  semester: 'semester',
-  student: 'student',
+const STATS_TYPES = {
+  PEOPLE: 'PEOPLE',
+  COURSES: 'COURSES',
+  SECTIONS: 'SECTIONS',
 };
 
-export const filters = [
-  {id: 1, name: 'Curso'},
-  {id: 2, name: 'Ciclo/Semestre'},
-  {id: 3, name: 'Estudiante'},
-];
+function StatsRender(type) {
+  const {colors} = useTheme();
+
+  const [peopleData, setPeopleData] = useState([
+    {x: 'Alumnos', y: 30},
+    {x: 'Catedraticos', y: 40},
+  ]);
+  const [coursesData, setCoursesData] = useState([
+    {y: 30, x: 'Desarrollo Web'},
+    {y: 23, x: 'Analisis de Sistemas'},
+    {y: 34, x: 'Redes de Computacion'},
+    {y: 42, x: 'Etica Profesional'},
+  ]);
+  const [sectionsData, setSectionsData] = useState([
+    {x: 'A', y: 30},
+    {x: 'B', y: 32},
+    {x: 'C', y: 35},
+    {x: 'D', y: 23},
+  ]);
+  const [courseSelected, setCourseSelected] = useState('Desarrollo Web');
+
+  const renderPeopleChart = () => {
+    return (
+      <ScrollView>
+        <Title style={{textAlign: 'center', marginTop: 30}}>Distribucion</Title>
+        <VictoryPie
+          data={peopleData}
+          labelRadius={({innerRadius}) => innerRadius + 10}
+          style={{labels: {fill: 'white', fontSize: 20}}}
+          labels={({datum}) => `${datum.x} ${datum.y}`}
+        />
+      </ScrollView>
+    );
+  };
+
+  const renderCoursesChart = () => {
+    return (
+      <ScrollView>
+        <Title style={{textAlign: 'center', marginTop: 30}}>
+          Alumnos por curso
+        </Title>
+        <VictoryChart domainPadding={25}>
+          <VictoryAxis dependentAxis />
+          <VictoryBar
+            data={coursesData}
+            categories={{
+              x: [
+                'Desarrollo Web',
+                'Analisis de Sistemas',
+                'Redes de Computacion',
+                'Etica Profesional',
+              ],
+            }}
+            labels={({datum}) => datum.y}
+            style={{labels: {fill: 'white'}}}
+            labelComponent={<VictoryLabel dy={30} />}
+          />
+        </VictoryChart>
+        <View style={{flex: 1, flexDirection: 'column', marginHorizontal: 30}}>
+          {coursesData.map(course => {
+            return (
+              <View style={{flexDirection: 'row', marginVertical: 2}}>
+                <Text style={{fontWeight: 'bold'}}>- {course.x} :</Text>
+                <Text>{course.y} estudiantes</Text>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
+    );
+  };
+
+  const renderSectionsChart = () => {
+    return (
+      <ScrollView>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Title style={{ textAlign: 'center', marginTop: 30}}>{courseSelected}</Title>
+          <VictoryPie
+            data={sectionsData}
+            labelRadius={({innerRadius}) => innerRadius + 70}
+            style={{labels: {fill: 'white', fontSize: 20}}}
+            labels={({datum}) => `Seccion ${datum.x}\n ${datum.y}`}
+          />
+        </View>
+      </ScrollView>
+    );
+  };
+
+  const renderCharts = () => {
+    switch (type.type) {
+      case STATS_TYPES.PEOPLE:
+        return renderPeopleChart();
+      case STATS_TYPES.COURSES:
+        return renderCoursesChart();
+      case STATS_TYPES.SECTIONS:
+        return renderSectionsChart();
+      default:
+        return <Text>The chart is undefined</Text>;
+    }
+  };
+
+  return renderCharts();
+}
 
 const StatsScreen = () => {
   const {colors} = useTheme();
   const backgroundColor = {backgroundColor: colors.appBackground};
 
-  const [filterSelected, setFilter] = useState();
-  const [lineCharData, setLineCharData] = useState({
-    labels: ['June'],
-    datasets: [
-      {
-        data: [Math.random() * 100],
-      },
-    ],
-  });
-  // const [courses, setCourses] = useState();
-  // const [semesters, setFilter] = useState();
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    let {data: attendance, error} = await supabase
-      .from('attendance')
-      .select('*');
-
-    // console.log('attendance', attendance);
-    setLineCharData(getLineCharData(filter_map.course, attendance || []));
-  };
-
   return (
     <View style={[styles.container, backgroundColor]}>
       <Appbar.Header style={styles.header}>
-        <Appbar.Content title="Estadísticas" color="white" />
+        <Appbar.Content title="Stats" color="white" />
       </Appbar.Header>
-      <View style={styles.content}>
-        <ListAccordion
-          title="Filtros"
-          listTitle={filterSelected?.name || 'Selecciona una opción.'}
-          options={filters}
-          onChange={setFilter}
-          // icon="alphabetical-variant"
-        />
-      </View>
-      <ScrollView style={styles.content}>
-        <View>
-          <Text>Hello from Stats screen</Text>
-        </View>
-        <View>
-          <Text>Bezier Line Chart</Text>
-          <LineChart
-            data={lineCharData}
-            width={screenWidth} // from react-native
-            height={220}
-            // yAxisLabel="$"
-            // yAxisSuffix="k"
-            yAxisInterval={1} // optional, defaults to 1
-            chartConfig={{
-              backgroundColor: '#e26a00',
-              backgroundGradientFrom: '#fb8c00',
-              backgroundGradientTo: '#ffa726',
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-              propsForDots: {
-                r: '6',
-                strokeWidth: '2',
-                stroke: '#ffa726',
-              },
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16,
-            }}
-          />
-          <ProgressChart
-            data={{
-              labels: ['Swim', 'Bike', 'Run'], // optional
-              data: [0.4, 0.6, 0.8],
-            }}
-            width={screenWidth}
-            height={220}
-            strokeWidth={16}
-            radius={32}
-            chartConfig={{
-              backgroundColor: '#022173',
-              backgroundGradientFrom: '#022173',
-              backgroundGradientTo: '#1b3fa0',
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            hideLegend={false}
-          />
-          <PieChart
-            data={[
-              {
-                name: 'Seoul',
-                population: 21500000,
-                color: 'rgba(131, 167, 234, 1)',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'Toronto',
-                population: 2800000,
-                color: '#F00',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'Beijing',
-                population: 527612,
-                color: 'red',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'New York',
-                population: 8538000,
-                color: '#ffffff',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-              {
-                name: 'Moscow',
-                population: 11920000,
-                color: 'rgb(0, 0, 255)',
-                legendFontColor: '#7F7F7F',
-                legendFontSize: 15,
-              },
-            ]}
-            width={screenWidth}
-            height={220}
-            chartConfig={{
-              backgroundColor: '#e26a00',
-              backgroundGradientFrom: '#fb8c00',
-              backgroundGradientTo: '#ffa726',
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            accessor={'population'}
-            backgroundColor={'transparent'}
-            paddingLeft={'15'}
-            center={[10, 50]}
-            absolute
-          />
-        </View>
-      </ScrollView>
+      <Tabs>
+        <TabScreen label="Personas" icon="account-multiple-outline">
+          <StatsRender type={STATS_TYPES.PEOPLE} />
+        </TabScreen>
+        <TabScreen label="Cursos" icon="apps">
+          <StatsRender type={STATS_TYPES.COURSES} />
+        </TabScreen>
+        <TabScreen label="Secciones" icon="ab-testing">
+          <StatsRender type={STATS_TYPES.SECTIONS} />
+        </TabScreen>
+      </Tabs>
     </View>
   );
 };
