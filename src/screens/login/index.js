c6263fe4fbc7bfad581bf7115e285f7b509fedb0
Gianfranco <gianfrancohj03@gmail.com>
2021-08-27 00:52:49 -0600

    :sparkles: Fix login styles

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { TextInput, Button, Card } from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import {View, ImageBackground, StatusBar} from 'react-native';
+import {TextInput, Button, Text} from 'react-native-paper';
 import RNBootSplash from 'react-native-bootsplash';
-import { isEmpty } from "lodash";
+import {isEmpty} from 'lodash';
 
-import { useUser } from '../../context/userContext';
-import { supabase } from '../../lib/constants/api';
-import showToast, { TYPES } from '../../utils/toast';
+import {useUser} from '../../context/userContext';
+import {supabase} from '../../lib/constants/api';
+import showToast, {TYPES} from '../../utils/toast';
 
+import colors from '../../lib/constants/colors';
 import styles from './styles';
 
 const LoginScreen = props => {
-  const { navigation } = props;
-  const { user } = useUser()
+  const {navigation} = props;
+  const {user} = useUser();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -24,7 +25,7 @@ const LoginScreen = props => {
     } else {
       navigation.reset({
         index: 0,
-        routes: [{ name: 'Main' }],
+        routes: [{name: 'Main'}],
       });
       setTimeout(() => {
         RNBootSplash.hide();
@@ -35,58 +36,68 @@ const LoginScreen = props => {
   const onSignIn = async () => {
     if (!isEmpty(email) || !isEmpty(password)) {
       try {
-        const { user, error } = await supabase.auth.signIn({ email, password });
+        const {user, error} = await supabase.auth.signIn({email, password});
         if (user) {
           navigation.replace('Main');
         }
         if (error) {
-          showToast({ text: error.message, type: TYPES.ERROR });
+          showToast({text: error.message, type: TYPES.ERROR});
         }
       } catch (e) {
-        showToast({ text: 'Algo salió mal, intente más tarde.', type: TYPES.ERROR });
+        showToast({
+          text: 'Algo salió mal, intente más tarde.',
+          type: TYPES.ERROR,
+        });
       }
     } else {
-      showToast({ text: 'Ingrese correo y contraseña.', type: TYPES.SUCCESS });
+      showToast({text: 'Ingrese correo y contraseña.', type: TYPES.SUCCESS});
     }
   };
 
+  const uri =
+    'https://images.unsplash.com/photo-1600195077077-7c815f540a3d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=636&q=80';
+
   return (
-    <View>
-
-    <Card style={ styles.containerForm }>
-    <Card.Title title="Iniciar Sesión" subtitle="Ingrese sus datos" style={ styles.textoTitulo } />
-    <Card.Content>
-
-      <TextInput
-      style={styles.inputText}
-      label="Email"
-      value={email}
-      placeholder="Ingrese su correo"
-      onChangeText={email => setEmail(email)}
-      />
-
-      <TextInput
-      style={styles.inputText}
-      label="Password"
-      value={password}
-      secureTextEntry={true}
-      placeholder="Contraseña"
-      onChangeText={password => setPassword(password)}
-      />
-      
+    <ImageBackground style={styles.container} resizeMode="cover" source={{uri}}>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.titleText}>Iniciar Sesión</Text>
+      <Text style={styles.subTitleText}>
+        Asistencia Universidad Mariano Gálvez de Guatemala
+      </Text>
+      <View style={styles.inputContainer} s>
+        <TextInput
+          mode="flat"
+          left={<TextInput.Icon name="email" color={colors.primary} />}
+          style={styles.inputText}
+          label="Email"
+          value={email}
+          placeholder="Ingrese su correo"
+          onChangeText={email => setEmail(email)}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          mode="flat"
+          left={<TextInput.Icon name="lock" color={colors.primary} />}
+          style={styles.inputText}
+          label="Password"
+          value={password}
+          secureTextEntry={true}
+          placeholder="Contraseña"
+          onChangeText={password => setPassword(password)}
+        />
+      </View>
       <Button
-      style={ styles.button }
-      icon="login"
-      mode="contained"
-      onPress={() => {
+        style={styles.button}
+        icon="login"
+        mode="contained"
+        labelStyle={{color: 'white'}}
+        onPress={() => {
           onSignIn();
         }}>
-          Iniciar Sesión
+        Iniciar Sesión
       </Button>
-    </Card.Content>
-
-    </Card>
-    </View>
+    </ImageBackground>
   );
 };
 
--- src/screens/login/styles.js
+++ src/screens/login/styles.js
@@ -1,36 +1,56 @@
-import {StyleSheet} from 'react-native';
-import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {StyleSheet, Platform} from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingHorizontal: wp(7),
   },
-  containerForm: {
-    textAlignVertical: 'center',
-    marginTop: hp(25),
+  titleText: {
+    marginBottom: hp(3),
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: wp(8),
+    textShadowColor: '#000',
+    textShadowOffset: {width: 5, height: 5},
+    textShadowRadius: 10,
+  },
+  subTitleText: {
+    marginBottom: hp(5),
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: wp(5),
+    textAlign: 'center',
+    textShadowColor: '#000',
+    textShadowOffset: {width: 5, height: 5},
+    textShadowRadius: Platform.OS === 'ios' ? 4 : 9,
+  },
+  inputContainer: {
+    borderRadius: wp(4),
+    height: hp(6.7),
+    overflow: 'hidden',
+    marginBottom: hp(4),
+  },
+  inputText: {
+    height: hp(7),
+    overflow: 'hidden',
+    borderRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    paddingLeft: wp(2),
   },
   button: {
-    marginTop: hp(3),
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: hp(1),
+    marginTop: hp(1),
+    paddingVertical: Platform.OS === 'ios' ? hp(1.2) : hp(0.5),
   },
   buttonText: {
     color: 'white',
   },
-  inputText: {
-    height: hp(9),
-    borderTopRightRadius: hp(3),
-    borderTopLeftRadius: hp(3),
-    borderRadius: hp(3),
-    marginBottom: hp(4)
-  },
-  textoTitulo: {
-    color: 'blue',
-    justifyContent: 'center'
-  }
 });
 
 export default styles;
