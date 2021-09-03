a93973e80925a51bbb460c0e2ea121fd61f07998
Gianfranco <gianfrancohj03@gmail.com>
2021-09-02 22:22:54 -0600

    Stats v1

--- src/lib/constants/colors.js
+++ src/lib/constants/colors.js
@@ -1,9 +1,10 @@
 const colors = {
   // primary: 'tomato',
-  primary: '#CE7E8A',
-  accent: '#EAB1A4',
+  // primary: '#CE7E8A',
+  // accent: '#EAB1A4',
+  primary: '#23265B',
   // primary: '#23265B',
-  // accent: '#72C0DD',
+  accent: '#CE7E8A',
   secondary: '#5A5A6F',
   tertiary: '#1A3252',
   text: 'black',
--- src/screens/stats/styles.js
+++ src/screens/stats/styles.js
@@ -1,10 +1,21 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export const containerWidth = Platform.OS === 'ios' ? hp(8) : hp(9);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  header: {
+    height: containerWidth,
+  },
+  content: {
+    // flex: 1,
+    paddingHorizontal: wp(5),
   },
 });
 
